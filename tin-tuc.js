9dd232098516237690756185a24ae43ae4450551
Ngo Quoc Hiep <drquochiep87@gmail.com>
2020-06-15 13:35:57 +0700

    first commit

---
+++ tin-tuc.js
@@ -0,0 +1,17 @@
+﻿
+  $(document).ready(function(){
+    $(".fa.fa-chevron-up").click(function(){
+      $(".social").show();
+      $(".footer-fixed").hide();
+    })
+  })
+
+
+  $(document).ready(function(){
+    $(".fa.fa-times").click(function(){
+      $(".social").hide();
+      $(".footer-fixed").show();
+    })
+  })
+
+  
